507e1e57740bc5509bf3cd3e9f6b6ddec1d32b0c
Muhammad  Lodhi <lodhik9@gmail.com>
2023-06-27 12:17:03 +0200

    Route Parameters in Symfony

---
+++ src/Controller/MoviesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MoviesController extends AbstractController
+{
+    #[Route('/movies/{name}', name: 'movies',defaults:['name'=>null], methods:['GET', 'HEAD'])]
+    public function index($name): Response
+    {
+        return $this->json([
+            'message' => $name,
+            'path' => 'src/Contoller/MoviesController.php'
+        ]);
+
+    }
+}
